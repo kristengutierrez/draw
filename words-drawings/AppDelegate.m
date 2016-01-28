6c17c3055c814c0ffd6a33ad2131701170519e7f
Matthew McClure <matthewmcclure@gmail.com>
2016-01-28 15:57:08 -0600

    added comment

--- words-drawings/AppDelegate.m
+++ words-drawings/AppDelegate.m
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   // Override point for customization after application launch.
-  
+  //made a change
 
   
   return YES;
